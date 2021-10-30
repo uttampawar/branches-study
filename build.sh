f0eb72af3fb02d5e2ab23f1f3c7f983441f1c9e2
Uttam Pawar <uttam.c.pawar@intel.com>
2021-10-29 17:06:56 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,16 @@
 flags="-fcf-protection=none -g -O2"
 flags="-DDEBUG -g -O2"
 flags="-g -O2"
+flags="-O3"
+echo "Compiling goto.c"
 gcc ${flags} -c goto.c
+echo "Compiling test-goto.c"
 gcc ${flags} -c test-goto.c
+echo "Compiling switch.c"
 gcc ${flags} -c switch.c
+echo "Compiling test-goto.c"
 gcc ${flags} -c test-switch.c
+echo "Linking goto_test from goto.o test-goto.o"
 gcc ${flags} goto.o test-goto.o -o goto_test
+echo "Linking switch_test from switch.o test-switch.o"
 gcc ${flags} switch.o test-switch.o -o switch_test
